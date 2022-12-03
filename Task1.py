77f68aca9664654eeb16a019b0272eac745d1f40
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-12-03 19:26:12 +0300

    Task1

---
+++ Task1.py
@@ -0,0 +1,8 @@
+task1 = input('Введите числа через пробел')
+task1 = task1.strip().split(' ')
+max_num = 0
+for i in range(len(task1)):
+    task1[i] = int(task1[i])
+    if i % 2 > 0:
+        max_num = max_num + task1[i]
+print(max_num)
